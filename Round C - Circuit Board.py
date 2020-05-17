a785950f61b408dfc25012fd95864505c9c339d8
avielchow <avielchow88@gmail.com>
2020-05-17 03:47:01 -0600

    Update Round C - Circuit Board.py

--- Round C - Circuit Board.py
+++ Round C - Circuit Board.py
@@ -1,105 +1,70 @@
 #This is time limit exceeded at the moment
 #https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050ff2/0000000000150aae
 
+from collections import deque
 import numpy as np
 
-def histogram_row_count(row, k):
-    count = 0
-    min = row[0]
-    max = min
-    for number in row:
-        if number < min:
-            min = number
-        elif number > max:
-            max = number
-
-        if max - min > k:
-            return count
-        else:
-            count += 1
-    return count
-
-# For each row, define the max length where conditions are met to develop a 'histogram'
-def gethistogram(board, k):
-    histogram = []
+def get_all_histograms(board):
+    histograms = []
     for row in board:
-        histogram.append((histogram_row_count(row,k)))
-    return histogram
+        histogram = []
+        start = row[0]
+        count = 0
+        for number in row:
+            if number == start:
+                count += 1
+            else:
+                histogram.append(count)
+                for x in reversed(range(1, count)):
+                    histogram.append(x)
+                start = number
+                count = 1
+        histogram.append(count)
+        for x in reversed(range(1,count)):
+            histogram.append(x)
+        histograms.append(histogram)
 
+    histograms = np.array(histograms).T.tolist()
+    return histograms
 
 # Python3 program to find maximum
 # rectangular area in linear time
-def max_area_histogram(histogram):
-    # This function calculates maximum rectangular area under given histogram with n bars
+def largestRectangleArea(heights):
+    stack = [-1]
+    maxArea = 0
 
-    # Create an empty stack. The stack holds indexes of histogram[] list.
-    # The bars stored in the stack are always in increasing order of  their heights.
-    stack = []
+    for i in range(len(heights)):
+        # we are saving indexes in stack that is why we comparing last element in stack
+        # with current height to check if last element in stack not bigger then
+        # current element
+        while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+            lastElementIndex = stack.pop()
+            maxArea = max(maxArea, heights[lastElementIndex] * (i - stack[-1] - 1))
+        stack.append(i)
 
-    max_area = 0  # Initialize max area
+    # we went through all elements of heights array
+    # let's check if we have something left in stack
+    while stack[-1] != -1:
+        lastElementIndex = stack.pop()
+        maxArea = max(maxArea, heights[lastElementIndex] * (len(heights) - stack[-1] - 1))
 
-    # Run through all bars of given histogram
-    index = 0
-    while index < len(histogram):
+    return maxArea
 
-        # If this bar is higher than the bar on top stack, push it to stack
-
-        if (not stack) or (histogram[stack[-1]] <= histogram[index]):
-            stack.append(index)
-            index += 1
-
-        # If this bar is lower than top of stack, then calculate area of rectangle with
-        # stack top as the smallest (or minimum height) bar.'i' is 'right index' for
-        # the top and element before top in stack is 'left index'
-        else:
-            # pop the top
-            top_of_stack = stack.pop()
-
-            # Calculate the area with histogram[top_of_stack] stack as smallest bar
-            area = (histogram[top_of_stack] *
-                    ((index - stack[-1] - 1)
-                     if stack else index))
-
-            # update max area, if needed
-            max_area = max(max_area, area)
-
-            # Now pop the remaining bars from
-    # stack and calculate area with every popped bar as the smallest bar
-    while stack:
-        # pop the top
-        top_of_stack = stack.pop()
-
-        # Calculate the area with histogram[top_of_stack] stack as smallest bar
-        area = (histogram[top_of_stack] *
-                ((index - stack[-1] - 1)
-                 if stack else index))
-
-        # update max area, if needed
-        max_area = max(max_area, area)
-
-        # Return maximum area under
-    # the given histogram
-    return max_area
-
-# This code is contributed
-# by Jinay Shah
-
-def iterate_circuit_board(circuit_board, C):
+def iterate_circuit_board_quick(board):
     ans = 0
-    for x in range(C):
-        board = circuit_board[:,x:]
-        histogram = gethistogram(board,K)
-        max_area = max_area_histogram(histogram)
+    histograms = get_all_histograms(board)
+    for histogram in histograms:
+        max_area = largestRectangleArea(histogram)
         if max_area > ans:
             ans = max_area
     return ans
+
 
 T = int(input())
 for test in range(1,T+1):
     R, C, K = [int(x) for x in input().split()]
     circuit_board = []
     for line in range(R):
-        circuit_board.append([int(x) for x in input().split()])
-    circuit_board = np.array(circuit_board)
-    print("Case #" + str(test) + ": " + str(iterate_circuit_board(circuit_board,C)))
+        circuit_board.append(deque([int(x) for x in input().split()]))
 
+    print("Case #" + str(test) + ": " + str(iterate_circuit_board_quick(circuit_board)))
